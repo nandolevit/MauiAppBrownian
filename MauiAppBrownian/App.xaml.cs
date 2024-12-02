7e0f7f67cef8502669cd05fce09ea28ca9f58933
Luiz Fernando Silva dos Santos <nando_levita12@hotmail.com>
2024-12-02 13:45:29 -0300

    Add project files.

---
+++ MauiAppBrownian/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppBrownian
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppBrownian/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBrownian
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
